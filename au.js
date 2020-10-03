e16f82932e9c629b2de2539ca1133a0c56f2879e
Rickokechil <72317615+Rickokechil@users.noreply.github.com>
2020-10-04 01:54:29 +0700

    Update au.js

--- au.js
+++ au.js
@@ -159,7 +159,7 @@ class Client {
         <center><button id="toggleButton" onclick="window.client.startBots(localStorage.getItem('botAmount'));" class="btn btn-success">Start Bots</button></center>
         `);
         if (!localStorage.getItem('botAmount')) localStorage.setItem('botAmount', 10);
-        if (!localStorage.getItem('botNick')) localStorage.setItem('botNick', 'Sanik');
+        if (!localStorage.getItem('botNick')) localStorage.setItem('botNick', 'SETAN');
         console.log('[AgarUnlimited] Ready!');
     }
 
